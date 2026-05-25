6506c4f7dbcaac31f677fb4127f4bd4c5e07900b
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-05-25 14:06:22 +0000

    Data Types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,19 @@
 console.log("Practising Data Types")
-// this code is running on node and not browser
+// this code is running on nodejs and not browser
+
+let name = "Javascript"
+let age = 18
+let isLoggedIn =true
+
+// number=> 2 to the power 53
+// bigger number use "Bigint" 
+// string=> "anything in alphabets"
+// boolean=> true/false
+// null=> standalone value
+// undefined => value not assigned
+// symbol=> unique
+
+// object
+console.log(typeof isLoggedIn)
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
