1d385e8534d381ceaec089094340941b81715856
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-05-26 06:23:24 +0000

    Coversion Operation on Data

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "20abc"
+console.log(typeof score)
+console.log(score)
+let valuInNumber= Number(score)
+console.log(typeof valuInNumber)
+console.log(valuInNumber)
+
+// "20" = 20
+// 20abc=> NaN
+// true = 1; false=0
+
+let isLoggedIn =1
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+console.log(booleanisLoggedIn)
+
+let UserisLoggedIn =""
+let booleanUserisLoggedIn= Boolean(UserisLoggedIn)
+console.log(typeof booleanUserisLoggedIn)
+console.log(booleanUserisLoggedIn)
