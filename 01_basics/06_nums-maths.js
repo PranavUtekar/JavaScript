33fef035e57afa9837fdf1b7ac4f855c89a12fc7
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-05-28 14:12:25 +0000

    Number_&_Maths

---
+++ 01_basics/06_nums-maths.js
@@ -0,0 +1,11 @@
+console.log("Number & Maths");
+const score = 100
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+
