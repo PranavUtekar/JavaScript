73d1ea1c002cee2b082e994e17594cbe7825edb5
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-05-29 06:39:02 +0000

    Maths and Numbers

--- 01_basics/06_nums-maths.js
+++ 01_basics/06_nums-maths.js
@@ -7,5 +7,30 @@ console.log(balance)
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 
+const number2 = 189.098
+console.log(number2.toPrecision(5));
+
+const number3 = 10000000
+console.log(number3.toLocaleString("en-IN"));
+
+
+//*****************************************  Maths    ****************************************
+
+console.log(Math);
+console.log(Math.abs(-55));
+console.log(Math.round(5.56));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(0.1));
+
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
+
+
+
 
 
