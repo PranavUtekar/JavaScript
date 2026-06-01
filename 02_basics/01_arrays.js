a99d78be163958a6097709d8a4298e446e761cca
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-01 13:43:47 +0000

    Arrays in JS

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,1 +1,31 @@
 console.log("Arrays in JS");
+
+const myArr = [0, 1, 2, 3, 4, 5] 
+
+console.log(myArr);
+
+myArr.push(6, 7, 8)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(-1)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(9));
+
+console.log(myArr.indexOf(-1));
+console.log(myArr.indexOf(0));
+
+console.log("A ",);
+
+
+
+
+
+
+
