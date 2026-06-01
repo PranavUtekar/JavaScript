b7341aff4b7158bd739631b241c2388d17665304
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-01 13:58:46 +0000

    Arrays in JS

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -21,7 +21,15 @@ console.log(myArr.includes(9));
 console.log(myArr.indexOf(-1));
 console.log(myArr.indexOf(0));
 
-console.log("A ",);
+console.log("A ",myArr);
+const myn1= myArr.slice(1,3)
+console.log(myn1);
+console.log("B ", myArr);
+const myn2= myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
 
 
 
