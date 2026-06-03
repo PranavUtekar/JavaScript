96657c664d4253fe3f1532ce1e66b7440d501bff
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-03 13:19:09 +0000

    Functions in Js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,12 @@
 
-function name(){
-    console.log("A");
-    console.log("b");
-    console.log("c");
-    console.log("d");
-    console.log("e");
+// function name(){
+//     console.log("A");
+//     console.log("b");
+//     console.log("c");
+//     console.log("d");
+//     console.log("e");
 
-}
+// }
 
 // name()
 
@@ -14,11 +14,22 @@ function name(){
 // function addtwoNumber(number1, number2){
 //     console.log(number1+number2)
 // }
-function addtwoNumber(number1, number2){
-    // let result = number1+number2
-    // return result
-    return number1+number2
-}
+// function addtwoNumber(number1, number2){
+//     // let result = number1+number2
+//     // return result
+//     return number1+number2
+// }
 
-const result =addtwoNumber(3,4)
-console.log("Result: ",result);
+// const result =addtwoNumber(3,4)
+// console.log("Result: ",result);
+
+function loginUserMessage(username="Anonynous"){
+    if(username===undefined){
+        console.log("Please enter a username");
+        
+
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("Sam"))
+console.log(loginUserMessage())
