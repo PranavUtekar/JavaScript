60783210b56a6b8a9e99f6adf29dc4f54dbe6c03
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-11 07:31:33 +0000

    Scope in Js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,4 @@
-console.log("Learning Scopes in Js");
+// console.log("Learning Scopes in Js");
 
 // if(true){
 //     let a =10 
@@ -7,4 +7,47 @@ console.log("Learning Scopes in Js");
 
 // }
 // console.log(a, b, c);
+
+// let a = 300
+
+// if(true){
+//     let a =30
+//     console.log("Inner: ",a);
+    
+// }
+
+// console.log("Outer: ", a);
+
+
+// function one(){
+//     const username = "Sam"
+//     function two(){
+//         const website = "Youtube"
+//         console.log(username);       
+//     } 
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+
+
+// if(true){
+//     const username = "Sammy"
+//     if(username == "Sammy"){
+//         const website = " Google"
+//         console.log(username + website);
+//     }
+// }
+
+console.log(addone(5))
+
+function addone(num){
+    return num +1
+}
+
+const addtwo = function (num){
+    return num +2
+}
+console.log(addtwo(10))
 
