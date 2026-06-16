48dc203f35916341e3e7459a118ce64ff5b3ddcd
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-16 14:18:24 +0000

    For of loop in Js

---
+++ 05_iterations/three.js
@@ -0,0 +1,43 @@
+// for of
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "Hello World!"
+
+// for (const greet of greetings) {
+//     if (greet==" ") {
+//         continue
+//     }
+//     console.log(`Each character is ${greet}`);
+    
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set("IN", "India")
+// map.set("FR", "France")
+// map.set("AF", "Africa")
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value);
+    
+// }
+
+
+// const myObject ={
+//     'Game1':'NFS',
+//     'Game2':'Spiderman'
+// }
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ":-", value);
+    
+// }
