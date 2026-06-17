ad44f35f899af12c9a147d7b5c3dc4e26c702cdd
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-17 13:30:30 +0000

    For Each loop in Js

---
+++ 05_iterations/five.js
@@ -0,0 +1,42 @@
+const coding = ["js", "rb", "py", "java", "cpp"]
+// coding.forEach( function (val) {
+//     console.log(val);
+    
+// } )
+
+// coding.forEach((val)=>{
+//     console.log(val);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item, index, arr);
+    
+// })
+
+
+const myCoding=[
+    {
+        languageName: "javascript",
+        languageFileName:"Js"
+    },
+    {
+        languageName: "Python",
+        languageFileName:"Py"
+    },
+    {
+        languageName: "Java",
+        languageFileName:"java"
+    },
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName);
+    
+})
