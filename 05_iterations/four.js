308497b539b686256cc03dd159dbb46132426908
PranavUtekar <131793056+PranavUtekar@users.noreply.github.com>
2026-06-17 13:31:08 +0000

    For In loop in Js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,10 +1,19 @@
-const myObject = {
-    js:"Javascript",
-    cpp:"C++",
-    rb:"Ruby",
-    py:"Python"
-}
-for (const key in myObject) {
-    console.log(`${key} is shortcut for ${myObject[key]}`);
+// const myObject = {
+//     js:"Javascript",
+//     cpp:"C++",
+//     rb:"Ruby",
+//     py:"Python"
+// }
+// for (const key in myObject) {
+//     // console.log(`${key} is shortcut for ${myObject[key]}`);
+    
+// }
+
+const programming = ["js", "cpp", "rb", "py", "java"]
+
+for (const key in programming) {
+    console.log(programming[key]);
     
 }
+
+// map is not iteratable in for in loop
